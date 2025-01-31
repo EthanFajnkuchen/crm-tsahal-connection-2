eaf697679ff453687eb0cc0a87c9239b0dc809ee
EthanMoveo <ethanf@moveo.co.il>
2025-01-31 09:53:19 +0200

    change text files to ts object

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { CURRENT_STATUS } from "./i18n/current-status";
 function App() {
   const [count, setCount] = useState(0);
 
---
+++ client/src/i18n/emergency-contact.ts
@@ -0,0 +1,17 @@
+export const RELATION = {
+  relation: [
+    { value: "Mère", displayName: "Mère" },
+    { value: "Père", displayName: "Père" },
+    { value: "Grand-Mère", displayName: "Grand-Mère" },
+    { value: "Grand-Père", displayName: "Grand-Père" },
+    { value: "Frère", displayName: "Frère" },
+    { value: "Sœur", displayName: "Sœur" },
+    { value: "Oncle", displayName: "Oncle" },
+    { value: "Tante", displayName: "Tante" },
+    { value: "Cousin/e", displayName: "Cousin/e" },
+    { value: "Femme", displayName: "Femme" },
+    { value: "Mari", displayName: "Mari" },
+    { value: "Ami/e", displayName: "Ami/e" },
+    { value: "Autre", displayName: "Autre" },
+  ],
+};
