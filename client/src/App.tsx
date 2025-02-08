fccd2470a0020b794df71877b02c32ebb72bfe56
EthanMoveo <ethanf@moveo.co.il>
2025-02-08 12:07:26 +0200

    fix: fix imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import DashboardCardsSection from "./components/app-components/dashboard-card-section/dashboard-card-section";
-import LastTenLeadSection from "./components/app-components/last-ten-leads-section/last-ten-leads-section";
-import { AppSidebar } from "./components/app-components/sidebar/app-sidebar";
+import DashboardCardsSection from "@/components/app-components/dashboard-card-section/dashboard-card-section";
+import LastTenLeadSection from "@/components/app-components/last-ten-leads-section/last-ten-leads-section";
+import { AppSidebar } from "@/components/app-components/sidebar/app-sidebar";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { HeaderMobile } from "@/components/app-components/header-mobile/header-mobile";
+import Header from "@/components/app-components/header/header";
+
 import { BrowserRouter as Router } from "react-router-dom";
-import { useIsMobile } from "./hooks/use-mobile";
-import { HeaderMobile } from "./components/app-components/header-mobile/header-mobile";
-import Header from "./components/app-components/header/header";
 
 function App() {
   const isMobile = useIsMobile();
--- client/src/components/app-components/header/Header.tsx
+++ client/src/components/app-components/header/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import useCurrentPageName from "@/hooks/use-current-page-name";
 import { SearchInput } from "@/components/app-components/search-input/search-input";
 import UserProfile from "@/components/app-components/user-profile/user-profile";
+
 const Header: React.FC = () => {
   const pageTitle = useCurrentPageName();
 
