fc43f7ca9fb883d71522046a7d811a03dfccc599
EthanMoveo <ethanf@moveo.co.il>
2025-02-25 10:49:57 +0200

    refactor: remove unuse improt

--- client/src/components/app-components/last-ten-leads-table/last-ten-leads-table.tsx
+++ client/src/components/app-components/last-ten-leads-table/last-ten-leads-table.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import type { ColumnDef } from "@tanstack/react-table";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/store/store";
