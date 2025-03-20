15dac4254550e21a14ae452cb811fba28a728c92
EthanMoveo <ethanf@moveo.co.il>
2025-03-20 22:25:51 +0200

    style: remove comments

--- client/src/i18n/expert-co-card.ts
+++ client/src/i18n/expert-co-card.ts
@@ -1,34 +1,5 @@
-import {
-  FileUser,
-  ClipboardList,
-  TrendingUp,
-  Shield,
-  UserCheck,
-  UserMinus,
-  UserX,
-  MapPin,
-  Globe,
-  GraduationCap,
-  Clock,
-  School,
-  Book,
-} from "lucide-react";
+import { FileUser, ClipboardList } from "lucide-react";
 
-import { inProgressColumns } from "@/table-columns/in-progress-columns";
-import { toTreatColumns } from "@/table-columns/to-treat-columns";
-import { MichveAlonColumns } from "@/table-columns/michve-alon-columns";
-import { UnityColumns } from "@/table-columns/unity-columns";
-import { ReleasedColumns } from "@/table-columns/released-columns";
-import { NoResponseColumns } from "@/table-columns/no-response.columns";
-import { AbandonBeforeServiceColumns } from "@/table-columns/abandon-before-service-columns";
-import { AbandonDuringServiceColumns } from "@/table-columns/abandon-during-service-columns";
-import { ResidentIsraelNoFrameworkColumns } from "@/table-columns/resident-israel-no-framework-columns";
-import { ProgramAfterBacColumns } from "@/table-columns/program-after-bac-columns";
-import { ProgramPreArmyColumns } from "@/table-columns/program-pre-army-columns";
-import { YoudAlephColumns } from "@/table-columns/youd-aleph-columns";
-import { YoudBethColumns } from "@/table-columns/youd-beth-columns";
-import { StudyingOlimColumns } from "@/table-columns/studying-olim-columns";
-import { StudyingTouristColumns } from "@/table-columns/studying-tourist-columns";
 import { expertConnectionColumns } from "@/table-columns/expert-co-lead-columns";
 
 export const EXPERT_CO_CARD_ITEMS = [
