28b3762c29e0187efce1e24f4f1cdb2e2ea7eccc
EthanMoveo <ethanf@moveo.co.il>
2025-06-22 12:49:32 +0300

    feat: implement Discussion module with controller, service, entity, and DTOs for managing discussions related to leads

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -9,6 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { typeOrmConfig } from './config/orm-config';
 import { LeadModule } from './modules/lead/lead.module';
+import { DiscussionModule } from './modules/discussion/discussion.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { JwtModule } from '@nestjs/jwt';
@@ -19,17 +20,18 @@ import { LoggerMiddleware } from './middlewares/logging.middleware';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(typeOrmConfig),
     LeadModule,
+    DiscussionModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'default_secret_key',
       signOptions: { expiresIn: '1h' },
     }),
   ],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-  ],
+  // providers: [
+  //   {
+  //     provide: APP_GUARD,
+  //     useClass: JwtAuthGuard,
+  //   },
+  // ],
 })
 export class AppModule implements NestModule, OnModuleInit {
   private readonly logger = new Logger(AppModule.name);
