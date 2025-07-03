0acb19e6f9bfd028dabe2d127e5088d32b923618
EthanMoveo <ethanf@moveo.co.il>
2025-07-03 22:54:21 +0300

    style: update AppLayout and LeadDetails components with background color, enhance HeaderDesktop with sticky positioning, and modify useCurrentPageName hook to display lead names

--- client/src/hooks/use-current-page-name.tsx
+++ client/src/hooks/use-current-page-name.tsx
@@ -1,14 +1,17 @@
 import { useLocation } from "react-router-dom";
 import { SIDEBAR_ITEMS } from "@/i18n/sidebar-items";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const useCurrentPageName = (): string => {
   const location = useLocation();
   const currentPath = location.pathname;
+  const { data: lead } = useSelector((state: RootState) => state.leadDetails);
 
   const currentPage = SIDEBAR_ITEMS.find((item) => item.link === currentPath);
 
-  if (currentPath.includes("/lead-details/")) {
-    return `Lead Details`;
+  if (currentPath.includes("/lead-details/") && lead) {
+    return `${lead.firstName} ${lead.lastName}`;
   }
 
   return currentPage ? currentPage.displayName : "Page inconnue";
--- client/src/pages/lead-details/LeadDetails.tsx
+++ client/src/pages/lead-details/LeadDetails.tsx
@@ -49,7 +49,7 @@ const LeadDetailsPage: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="sticky top-0 z-10 bg-white border-b border-gray-200 shadow-sm">
+      <div className="sticky top-[6.5rem] lg:top-16 z-10 bg-white border-b border-gray-200 shadow-sm">
         <div className="max-w-7xl mx-auto px-4">
           <div className="flex space-x-1 overflow-x-auto scrollbar-hide py-2">
             {tabs.map((tab) => (
@@ -73,7 +73,7 @@ const LeadDetailsPage: React.FC = () => {
         </div>
       </div>
 
-      <div className=" mx-auto p-4 space-y-6">
+      <div className=" mx-auto p-4 space-y-6 bg-[#f3eefb]">
         {lead && (
           <>
             <div
