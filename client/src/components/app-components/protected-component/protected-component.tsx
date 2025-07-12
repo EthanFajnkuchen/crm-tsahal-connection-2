1e09c952746970c2bef6197ab3fa3bc7cd60e6c5
EthanMoveo <ethanf@moveo.co.il>
2025-07-12 22:41:48 +0300

    refactor: remove unused import of useUserPermissions in ProtectedComponent for cleaner code

--- client/src/components/app-components/protected-component/protected-component.tsx
+++ client/src/components/app-components/protected-component/protected-component.tsx
@@ -1,7 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { RoleType } from "@/types/role-types";
 import { ReactNode } from "react";
-import { useUserPermissions } from "@/hooks/use-user-permissions";
 
 interface ProtectedComponentProps {
   children: ReactNode;
