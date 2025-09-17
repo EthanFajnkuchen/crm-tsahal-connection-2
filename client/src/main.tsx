2857948dc653fdf3a3714cd4dd9b5e83f6ed07ee
EthanMoveo <ethanf@moveo.co.il>
2025-09-17 09:33:46 +0300

    Revert "Merge pull request #81 from EthanFajnkuchen/feature/rollback-fix"
    This reverts commit cdb9df465e33c1d7ec16a773c35b4ef89df2e9bf, reversing
    changes made to d1a5f900398434e555259f1be4f82fe5b082c2b0.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
+
 import { Provider } from "react-redux";
 import { store } from "@/store/store.ts";
 import AuthWrapper from "./AuthWrapper.tsx";
