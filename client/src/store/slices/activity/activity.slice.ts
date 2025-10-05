01eaadfd6a869cdb1f35aa242c7cc4eaccf9a804
EthanMoveo <ethanf@moveo.co.il>
2025-10-05 13:30:26 +0300

    refactor: remove unused PayloadAction import from activity slice

--- client/src/store/slices/activity/activity.slice.ts
+++ client/src/store/slices/activity/activity.slice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { Activity } from "@/store/adapters/activity/activity.adapter";
 import {
   createActivityThunk,
